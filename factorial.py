097ea27bd0cebb849c8d660f793912cd5efeab94
jashuva001 <jashuvabanavathu@gmail.com>
2026-01-06 17:21:30 +0530

    factorial.py

--- factorial.py
+++ factorial.py
@@ -1,15 +1,15 @@
-def factorial(n):
-    if n < 0:
-        raise ValueError("n must be >= 0")
+def factorial(a):
+    if a < 0:
+        raise ValueError("a must be >= 0")
     result = 1
-    for i in range(2, n+1):
+    for i in range(2, a+1):
         result *= i
     return result
 
 def main():
     try:
-        n = int(input("Enter a non-negative integer: "))
-        print(f"{n}! = {factorial(n)}")
+        a = int(input("Enter a non-negative integer: "))
+        print(f"{a}! = {factorial(a)}")
     except ValueError as e:
         print(f"Invalid input: {e}")
 
