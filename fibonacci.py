44e1c9b102cae09fc3149b570d8aea1fbd269fee
jashuva001 <jashuvabanavathu@gmail.com>
2026-01-08 16:55:19 +0530

    fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,19 +1,19 @@
-def fibonacci(n):
+def fibonacci(num):
     seq = []
-    a, b = 0, 1
-    for _ in range(n):
-        seq.append(a)
-        a, b = b, a + b
+    x, y = 0, 1
+    for _ in range(num):
+        seq.append(x)
+        x, y = y, x + y
     return seq
 
 def main():
     try:
-        n = int(input("How many Fibonacci numbers? "))
-        if n < 0:
-            raise ValueError("n must be >= 0")
-        print(fibonacci(n))
-    except ValueError as e:
-        print(f"Invalid input: {e}")
+        num = int(input("How many Fibonacci numbers? "))
+        if num < 0:
+            raise ValueError("num must be >= 0")
+        print(fibonacci(num))
+    except ValueError as error:
+        print(f"Invalid input: {error}")
 
 if __name__ == "__main__":
     main()
